679b195ba5f5d30a8469cf48ea7e7415e92aa119
zhaojhweb <141049587+zhaojhweb@users.noreply.github.com>
2024-07-16 16:51:25 +0800

    Update mod_PCA.R

--- R/mod_PCA.R
+++ R/mod_PCA.R
@@ -164,7 +164,7 @@ mod_pca_server <- function(input, output, session) {
         selectInput(
           inputId = ns("PCA_ellipse_type"),
           label = h4(HTML('<i class="fa fa-circle" aria-hidden="true"></i> <font size="3" color="black"><b>Ellipse type</b></font>')),
-          choices = c("confidence", "convex", "euclid", "norm", "t"),
+          choices = c("confidence", "convex", "norm", "t"),
           selected = "confidence"
         ) 
       )
